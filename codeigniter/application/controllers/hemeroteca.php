804e400b72c5779bd29092e481991f10aa6947b6
MariQuiroz <MARITZA@PC2.(none)>
2024-08-12 01:40:48 -0400

    cambio del nombre del nombre del controlador de todo minúscula a mayúscula al empezar el nombre

--- codeigniter/application/controllers/hemeroteca.php
+++ codeigniter/application/controllers/hemeroteca.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class hemeroteca extends CI_Controller {
+class Hemeroteca extends CI_Controller {
 	public function mostrar(){
 
 		$this->load->view('inc/vistaslte/head');
